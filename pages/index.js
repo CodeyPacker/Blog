dd4b6dd4c44ae701e58948dfeb6e4249e54f14e9
Cody Packer <codeypacker@gmail.com>
2021-01-26 00:30:23 -0700

    Connected to Contentful
    Connected to Contentful
    Removed default styles

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,49 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
 
-export default function Home() {
+import { fetchEntries } from "@utils/contentfulPosts";
+
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import Post from "@components/Post";
+
+export default function Home({ posts }) {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Next + Contentful Starter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header />
+        <div className="posts">
+          {posts.map((p) => {
+            return (
+              <Post
+                key={p.date}
+                date={p.date}
+                image={p.image.fields}
+                title={p.title}
+              />
+            );
+          })}
+        </div>
       </main>
 
       <Footer />
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetchEntries();
+  const posts = await res.map((p) => {
+    return p.fields;
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }

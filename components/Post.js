652e44ff732047bbef6ac999e705306614228e4b
Cody Packer <codeypacker@gmail.com>
2021-01-29 15:12:09 -0700

    Added a link to each single post from the homepage

--- components/Post.js
+++ components/Post.js
@@ -1,4 +1,7 @@
-function Post({ date, image, title }) {
+import { useRouter } from "next/router"
+
+function Post({ date, image, title, index }) {
+  const router = useRouter();
   let { file, description } = image;
 
   return (
@@ -7,6 +10,7 @@ function Post({ date, image, title }) {
       <p className="description">{description}</p>
       <h2>{title}</h2>
       <h3>{date.substring(0, 10)}</h3>
+      <span onClick={() => router.push(`/post/${index}`)}>Read me...</span>
     </div>
   );
 }

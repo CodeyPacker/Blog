de2c7e7cae766697d7582b69ca32d117dfdfcc33
Cody Packer <codeypacker@gmail.com>
2021-01-30 18:31:43 -0700

    Styled individual Posts

--- components/Post.js
+++ components/Post.js
@@ -1,16 +1,19 @@
 import { useRouter } from "next/router"
+import styles from './Post.module.scss'
 
 function Post({ date, image, title, index }) {
   const router = useRouter();
   let { file, description } = image;
 
   return (
-    <div className="post">
-      <img alt={description} src={`https:${file.url}`} />
-      <p className="description">{description}</p>
-      <h2>{title}</h2>
-      <h3>{date.substring(0, 10)}</h3>
-      <span onClick={() => router.push(`/post/${index}`)}>Read me...</span>
+    <div className={styles.post}>
+      <img className={styles.postImage} alt={description} src={`https:${file.url}`} />
+      <div className={styles.postDetails}>
+        <h2>{title}</h2>
+        <h3>{date.substring(0, 10)}</h3>
+        <p className="description">{description}</p>
+        <span onClick={() => router.push(`/post/${index}`)}>Read me...</span>
+      </div>
     </div>
   );
 }

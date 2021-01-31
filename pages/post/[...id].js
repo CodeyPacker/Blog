dcf0e4ba074ed805a101930675bc01f5a6b2814d
Cody Packer <codeypacker@gmail.com>
2021-01-31 00:20:11 -0700

    CPulled content into the 'single post' pages

--- pages/post/[...id].js
+++ pages/post/[...id].js
@@ -1,21 +1,26 @@
-import Post from "@components/Post";
 import { useRouter } from "next/router"
 import { fetchEntries } from "@utils/contentfulPosts";
+import styles from "@components/Post.module.scss";
 
 export default function singlePost({posts}) {
   const router = useRouter();
   const { id } = router.query;
   const currentPost = posts[`${id}`];
+  // Get the paragraphs from the Rich Text Content
+  // TODO: use the rich-text-to-react package, which makes getting all content types easy
+  const content = currentPost.article.content.map((n) => n.nodeType === 'paragraph' && n.content[0].value)
 
   return (
-    <div>
-      <Post
-        key={currentPost.date}
-        date={currentPost.date}
-        image={currentPost.image.fields}
-        title={currentPost.title}
-      />
-    </div>
+    <>
+      <section className={styles.hero}>
+        <img className={styles.heroImg}  alt={currentPost.description} src={`https:${currentPost.image.fields.file.url}`} />
+      </section>
+      <section className="constraint">
+        <h2>{currentPost.title}</h2>
+        <span>{currentPost.date.substring(0, 10)}</span>
+        {content.map(txt => <p>{txt}</p>)}
+      </section>
+    </>
   )
 }
 

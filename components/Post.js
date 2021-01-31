2368efdfe48111a2aeee87124a8d6fe9654ef6b2
Cody Packer <codeypacker@gmail.com>
2021-01-31 00:46:59 -0700

    Added post intro snippet

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,10 @@
 import { useRouter } from "next/router"
 import styles from './Post.module.scss'
 
-function Post({ date, image, title, index }) {
+function Post({ date, image, title, index, article}) {
   const router = useRouter();
   let { file, description } = image;
+  const content = article.content.map((n) => n.nodeType === 'paragraph' && n.content[0].value).map(p => p.substring(0, 50))
 
   return (
     <div className={styles.post}>
@@ -11,7 +12,7 @@ function Post({ date, image, title, index }) {
       <div className={styles.postDetails}>
         <h2>{title}</h2>
         <h3>{date.substring(0, 10)}</h3>
-        <p className="description">{description}</p>
+        <p className="description">{content[0]}...</p>
         <span onClick={() => router.push(`/post/${index}`)}>View post...</span>
       </div>
     </div>

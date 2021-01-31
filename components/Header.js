5f34ac7cc5796a5161cca3537c2e96b2be4740e1
Cody Packer <codeypacker@gmail.com>
2021-01-31 01:06:51 -0700

    Removed Header files

--- components/Header.js
+++
@@ -1,9 +0,0 @@
-import styles from './Header.module.scss'
-
-export default function Header({ title }) {
-  return (
-    <div className={styles.header}>
-      <span className={styles.logo, 'constraint'}>{title}</span>
-    </div>
-  )
-}

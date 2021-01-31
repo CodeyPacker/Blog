4770b98adba2e555af07a74c1f33232ebe0f7ce2
Cody Packer <codeypacker@gmail.com>
2021-01-31 00:20:38 -0700

    Added global font family

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,8 @@ export default function Home({ posts }) {
       <Head>
         <title>Next + Contentful Blog</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;1,300&display=swap" rel="stylesheet"/>
       </Head>
 
       <main>
